26b458ad98f1cf46afab81f3197d4e4003709237
DESKTOP-Q73OI6G\sj101 <seotjae@gmail.com>
2022-07-25 20:34:53 +0900

    jdbc dbcp mybatis 설정

---
+++ 01_BlogService/src/main/java/com/toy/blog/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.toy.blog.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		logger.info("홈페이지 접속");
+
+		return "home";
+	}
+}
